bfb4c93013215913893a3b83ba43f7acd68d9ca0
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-12-06 18:41:04 +0200

    Create a foreach iteration to calculate Sophia's grade

--- guided-project-arrays-iteration-selection/3-exercise-create-arrays-foreach-loops/GuidedProject/Starter/Program.cs
+++ guided-project-arrays-iteration-selection/3-exercise-create-arrays-foreach-loops/GuidedProject/Starter/Program.cs
@@ -1,33 +1,4 @@
-﻿using System;
-
-// initialize variables - graded assignments 
-int currentAssignments = 5;
-
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
-
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
-
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
-
-int sophiaSum = 0;
+﻿int sophiaSum = 0;
 int andrewSum = 0;
 int emmaSum = 0;
 int loganSum = 0;
@@ -46,12 +17,3 @@ sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
 emmaScore = (decimal)emmaSum / currentAssignments;
 loganScore = (decimal)loganSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
-
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
