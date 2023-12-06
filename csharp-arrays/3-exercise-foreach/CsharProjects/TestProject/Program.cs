1d652c6c5f34f61b8986ddba8777588ed40c5a64
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-12-06 16:23:18 +0200

    Create and initialize an array of int Add a foreach statement to iterate through the array Add a variable to sum the value of each element in the array Display the final value of sum

--- csharp-arrays/3-exercise-foreach/CsharProjects/TestProject/Program.cs
+++ csharp-arrays/3-exercise-foreach/CsharProjects/TestProject/Program.cs
@@ -1,5 +1,8 @@
-﻿string[] names = { "Rowena", "Robin", "Bao" };
-foreach (string name in names)
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+foreach (int items in inventory)
 {
-    Console.WriteLine(name);
+    sum += items;
 }
+
+Console.WriteLine($"We have {sum} items in inventory.");
